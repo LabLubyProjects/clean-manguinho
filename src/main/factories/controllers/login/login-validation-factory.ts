f6836d857cb03e2b275a571a829a09a792f7080c
Breno Vergilio <bjvergilio47@gmail.com>
2022-08-22 10:02:18 -0400

    feat: add AddSurvey composition

--- src/main/factories/controllers/login/login-validation-factory.ts
+++ src/main/factories/controllers/login/login-validation-factory.ts
@@ -1,6 +1,7 @@
 import { Validation } from '../../../../presentation/protocols/validation'
 import { ValidationComposite, RequiredFieldValidation, EmailValidation } from '../../../../validation/validators'
 import { EmailValidatorAdapter } from '../../../../infra/validators/email-validator-adapter'
+
 export const makeLoginValidation = (): ValidationComposite => {
   const validations: Validation[] = []
   for (const field of ['email', 'password']) {
